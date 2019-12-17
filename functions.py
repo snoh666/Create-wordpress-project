7df5e4d9b38dc88a04565a08259c73db49719cae
Mikolaj Snochowski <snochopl@gmail.com>
2019-12-17 15:23:12 +0100

    Added convertProps funcion

--- functions.py
+++ functions.py
@@ -18,3 +18,28 @@ def getWordpressVersions():
       return False
 
   return False
+
+
+def convertProps(props):
+
+  # Parametrs: 1=name, 2=version
+  correctProps = []
+
+  if len(props) == 0:
+    correctProps.append('default-prj-name')
+  elif len(props) == 1:
+    # I suppose name is correct - later i'll try to create checking funtion
+    correctProps.append(props[0])
+  elif len(props) == 2:
+    # I suppose name is correct - later i'll try to create checking funtion
+    correctProps.append(props[0])
+    if '@' in props[1]:
+      correctProps.append(props[1])
+    else:
+      print('Not correct wp version. Use "@" at the start.')
+      exit()
+  else:
+    print('Something went wrong with props converting')
+    exit()
+
+  return correctProps
