3aa2058e5366122dc665b1a9b2a9388381ea43ee
Mikolaj Snochowski <snochopl@gmail.com>
2019-12-17 15:06:34 +0100

    MVP of getting wordpress versions

--- index.py
+++ index.py
@@ -1,8 +1,24 @@
+# Importing functions
 import sys
 
-props = sys.argv
+from functions import getLatestWordpressDir
 
+# Importing const variables
+from props import projectsDirectory
+
+props = sys.argv
 # Pop the first item cause its filename
 props.pop(0)
 
 print(props)
+
+wordpressVersions = getLatestWordpressDir()
+
+if wordpressVersions == False:
+  print('No wordpress files avaible')
+  exit()
+else:
+  print('Avaible files')
+  print(wordpressVersions)
+  # Do everything correct => copy files with proper new name
+  # then create proper file structure inside wp-content themes
